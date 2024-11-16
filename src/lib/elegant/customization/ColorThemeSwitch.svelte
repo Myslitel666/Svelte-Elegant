7272c001f40be2e8937c645d263b498a3d5bf911
Myslitel666 <tretyakov_02@bk.ru>
2024-11-16 11:53:12 +0500

    Added a default dark theme.

--- src/lib/elegant/customization/ColorThemeSwitch.svelte
+++ src/lib/elegant/customization/ColorThemeSwitch.svelte
@@ -1,6 +1,7 @@
 <div>
     <Switch 
         onClick = {toggleThemeMode}
+        bind:isChecked = {isChecked}
         {...$$props}
     >
         <ActiveIcon slot="activeIcon" size="1.1rem" />
@@ -13,4 +14,14 @@
     import Switch from "../input/Switch.svelte";
     import ActiveIcon from '../../icons-elegant/Dark.svelte';
     import DisabledIcon from '../../icons-elegant/Light.svelte';
+    import { onMount } from 'svelte';
+    import { themeStore } from '../../stores/ColorThemeStore.js';
+
+    export let isChecked = false;
+
+    onMount(() => {
+        if (isChecked) {
+            toggleThemeMode();
+        }
+    });
 </script>
