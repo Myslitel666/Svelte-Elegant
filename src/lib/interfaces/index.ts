94c2be5c953488cf7afb0cc5cb449565fdf3a7c8
Myslitel666 <tretyakov_02@bk.ru>
2024-11-17 22:26:20 +0500

    Added export of interfaces and repositories.

---
+++ src/lib/interfaces/index.ts
@@ -0,0 +1,5 @@
+/* This file allows you to implement a named import. */
+/* For Example, import type { IColorThemeStore } from 'svelte-elegant'; */
+
+/* Interfaces */
+export type { IColorThemeStore } from './color-theme/IColorThemeStore.js';
---
+++ src/lib/stores/index.ts
@@ -0,0 +1,6 @@
+/* This file allows you to implement a named import. */
+/* For Example, import { themeStore, themeMode } from 'svelte-elegant'; */
+
+/* Stores */
+export { themeStore } from './ColorThemeStore.js';
+export { themeMode } from './ColorThemeStore.js';
