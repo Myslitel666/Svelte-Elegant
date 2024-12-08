b0a352a0af149620d0d9a08eb5cdbfe371d8f7dc
Myslitel666 <tretyakov_02@bk.ru>
2024-12-08 14:05:39 +0500

    Added export ElementIdStore

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -2,5 +2,6 @@
 /* For Example, import { themeStore, themeMode } from 'svelte-elegant'; */
 
 /* Stores */
+export { elementId, generateIdElement } from './ElementIdStore.js';
 export { themeStore } from './ColorThemeStore.js';
 export { themeMode } from './ColorThemeStore.js';
