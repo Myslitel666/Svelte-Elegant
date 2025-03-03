aba1984f9d591671f65b9f84ee7d7923b51053ea
Myslitel666 <tretyakov_02@bk.ru>
2025-03-03 23:04:01 +0500

    svelte2tsx

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', //ability to connect to the server from a mobile device
+		port: 5173 // Укажите другой порт
+	}
 });
