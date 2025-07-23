6a8bb5f315be4e2f2763d9f9ac704b61a27e2311
Myslitel666 <tretyakov_02@bk.ru>
2025-07-23 23:19:50 +0500

    Addded Plus.

--- src/lib/icons-elegant/PlusSquareFill.svelte
+++ src/lib/icons-elegant/PlusSquareFill.svelte
@@ -13,16 +13,17 @@
 </script>
 
 <svg
-  data-source="https://icons.getbootstrap.com/icons/plus/"
+  data-source="https://icons.getbootstrap.com/icons/plus-square-fill/"
   data-license="MIT"
   xmlns="http://www.w3.org/2000/svg"
   width={size}
   height={size}
   fill={fill || theme.palette.primary}
-  class="bi bi-plus"
+  class="bi bi-plus-square-fill"
   viewBox="0 0 16 16"
+  cursor="pointer"
 >
   <path
-    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
+    d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"
   />
 </svg>
--- src/lib/icons-elegant/index.js
+++ src/lib/icons-elegant/index.js
@@ -16,5 +16,6 @@ export { default as Light } from './Light.svelte';
 export { default as ModalIcon } from './ModalIcon.svelte';
 export { default as Notepad } from './Notepad.svelte';
 export { default as Plus } from './Plus.svelte';
+export { default as PlusSquareFill } from './PlusSquareFill.svelte';
 export { default as TableIcon } from './TableIcon.svelte';
 export { default as TriangularArrowDown } from './TriangularArrowDown.svelte';
