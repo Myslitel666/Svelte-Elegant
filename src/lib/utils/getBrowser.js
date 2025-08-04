690b5d1534aa9136fe22ef070bf202f5ff097dcc
Myslitel666 <tretyakov_02@bk.ru>
2025-08-04 23:02:17 +0500

    getBrowser

--- src/lib/utils/getBrowser.js
+++ src/lib/utils/getBrowser.js
@@ -1,7 +1,9 @@
 export const getBrowser = () => {
 	const userAgent = navigator.userAgent.toLowerCase();
 	const words = userAgent.split(" ");
-	const lastWord = words[words.length - 1];
+	let lastWord = words[words.length - 1];
+
+	if (lastWord.includes('safari') && words.length > 1) lastWord = words[words.length - 2];
 
 	//Применяем thin scroll для всех, кроме Opera
 	if (lastWord.includes('opera') || lastWord.includes('opr')) {
@@ -13,11 +15,11 @@ export const getBrowser = () => {
     else if (lastWord.includes('chrome')) {
 		return 'chrome';
 	}
-    else if (lastWord.includes('firefox')) {
+    else if (lastWord.includes('edg')) {
 		return 'edge';
 	}
-    else if (lastWord.includes('firefox')) {
-		return 'yandex';
+    else if (lastWord.includes('yabrowser')) {
+		return 'yabrowser';
 	}
 
     return lastWord;
