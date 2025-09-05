d9a0f9605468fb552d188d584e824c7f611e67bf
Myslitel666 <tretyakov_02@bk.ru>
2025-09-05 19:09:25 +0500

    i

--- src/lib/elegant/input/TextField.svelte
+++ src/lib/elegant/input/TextField.svelte
@@ -295,7 +295,7 @@
     style:--Xl-labelColor={xLabelColor}
     style:--Xl-labelColorHover={xLabelColorHover}
     style:--Xl-liftingHeight={variant === "Outlined"
-      ? `${height}/2 + 0.9*var(--Xl-activeborderWidth)`
+      ? `${height}/2 + 0.8 * var(--Xl-activeborderWidth)`
       : variant === "Standard"
         ? `${height}/2 + 0.4rem`
         : `${height}/2 + 0.9rem`}
