62b867aa62e2818b4563385b907800a1b2172272
Myslitel666 <tretyakov_02@bk.ru>
2025-09-06 12:43:39 +0500

    i

---
+++ src/lib/icons-elegant/Pause.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { themeStore } from "$lib/stores/ThemeStore.js";
+  import "./style/icons.css";
+
+  export let fill = "";
+  export let size = "70px";
+
+  let theme: any;
+
+  // Подписываемся на изменения темы
+  themeStore.subscribe((value) => {
+    theme = value;
+  });
+</script>
+
+<svg
+  fill={fill || theme.palette.text.main}
+  width={size}
+  height={size}
+  viewBox="0 0 32 32"
+  version="1.1"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <title>pause</title>
+  <path
+    d="M5.92 24.096q0 0.832 0.576 1.408t1.44 0.608h4.032q0.832 0 1.44-0.608t0.576-1.408v-16.16q0-0.832-0.576-1.44t-1.44-0.576h-4.032q-0.832 0-1.44 0.576t-0.576 1.44v16.16zM18.016 24.096q0 0.832 0.608 1.408t1.408 0.608h4.032q0.832 0 1.44-0.608t0.576-1.408v-16.16q0-0.832-0.576-1.44t-1.44-0.576h-4.032q-0.832 0-1.408 0.576t-0.608 1.44v16.16z"
+  ></path>
+</svg>
