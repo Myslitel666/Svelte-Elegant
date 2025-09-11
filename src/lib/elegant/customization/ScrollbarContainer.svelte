f7a90ed0b630c0160fac339abd595efd7139acc3
Myslitel666 <tretyakov_02@bk.ru>
2025-09-11 21:04:37 +0500

    i

--- src/lib/elegant/customization/ScrollbarContainer.svelte
+++ src/lib/elegant/customization/ScrollbarContainer.svelte
@@ -4,11 +4,13 @@
   import "../../font.css";
 
   export let height = "";
+  export let maxHeight = "";
   export let width = "100%";
 </script>
 
 <div
   style:height={height ? height : `calc(100dvh - ${$headerHeight})`}
+  style:max-height={maxHeight}
   style:width
   class="app scrollable-content"
 >
